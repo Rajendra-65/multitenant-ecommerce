ce17979e2265a375771b1df020f894104cbc2168
Rajendra-65 <Rajendrbehera9@gmail.com>
2025-05-06 13:17:13 +0530

    Adding all the components from shadcn and modifying some base ones

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import { Button } from "@/components/ui/button";
 import { Checkbox } from "@/components/ui/checkbox";
 import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
 
 export default function Home() {
   return (
@@ -14,18 +16,19 @@ export default function Home() {
           </Button>
         </div>
         <div>
-          <Input placeholder='I am an input' />
+          <Input className="border" placeholder='I am an input' />
+        </div>
+        <div >
+          <Progress value={50} className='w-full'/>
         </div>
         <div>
-          <progress value={50} />
+          <Textarea  className='w-full border rounded-b-sm' placeholder="I am a textarea w-full" />
         </div>
         <div>
-          <textarea placeholder="I am a textarea" />
-        </div>
-        <div>
-          <Checkbox aria-placeholder="hii" />
+          <Checkbox className = 'border-2 ' aria-placeholder="hii" />
         </div>
       </div>
     </div>
+    // <h6 className="text-9xl">Hello</h6>
   );
 }
