d593596338a195e5c191813a8c9dc8c783fb166b
Rajendra-65 <Rajendrbehera9@gmail.com>
2025-05-06 12:01:37 +0530

    Adding the button and other input components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,31 @@
-
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
 
 export default function Home() {
   return (
-    <div className = 'font-bold'>
-      Hello how are you
+    <div className="p-4">
+      <div className='flex flex-col gap-y-4 '>
+        <div>
+          <Button
+            variant="elevated"
+          >
+            I am a button
+          </Button>
+        </div>
+        <div>
+          <Input placeholder='I am an input' />
+        </div>
+        <div>
+          <progress value={50} />
+        </div>
+        <div>
+          <textarea placeholder="I am a textarea" />
+        </div>
+        <div>
+          <Checkbox aria-placeholder="hii" />
+        </div>
+      </div>
     </div>
   );
 }
