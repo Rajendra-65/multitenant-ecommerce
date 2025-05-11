3ac6356dd248cffabae3baa5ff2f4568544741ee
Rajendra-65 <Rajendrbehera9@gmail.com>
2025-05-11 13:11:40 +0530

    Fixing the superjons issue in trpc

--- src/trpc/init.ts
+++ src/trpc/init.ts
@@ -2,7 +2,7 @@ import { initTRPC } from '@trpc/server';
 import { cache } from 'react';
 import { getPayload } from 'payload';
 import config from "@payload-config"
-
+import superjson from "superjson"
 export const createTRPCContext = cache(async () => {
   /**
    * @see: https://trpc.io/docs/server/context
@@ -20,6 +20,7 @@ const t = initTRPC.create({
    * @see https://trpc.io/docs/server/data-transformers
    */
   // transformer: superjson,
+  transformer : superjson
 });
 
 // Base router and procedure helpers
