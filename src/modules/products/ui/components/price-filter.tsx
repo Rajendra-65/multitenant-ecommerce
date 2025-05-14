41676e0ff7719b6ab729876f551628cfd65cfca9
Rajendra-65 <Rajendrbehera9@gmail.com>
2025-05-14 16:14:14 +0530

    Adding nuqs and setting up the price filter option UI on category page

---
+++ src/modules/products/ui/components/price-filter.tsx
@@ -0,0 +1,73 @@
+"use client";
+
+import { ChangeEvent } from "react";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+
+interface Props {
+  minPrice?: string | null;
+  maxPrice?: string | null;
+  onMinPriceChange: (value: string) => void;
+  onMaxPriceChange: (value: string) => void;
+}
+
+export const formatAsCurrency = (value: string) => {
+  const numericValue = value.replace(/[^0-9.]/g, "");
+
+  const parts = numericValue.split(".");
+  const formattedValue =
+    parts[0] + (parts.length > 1 ? "." + parts[1]?.slice(0, 2) : "");
+
+  if (!formattedValue) return "";
+
+  const numberValue = parseFloat(formattedValue);
+
+  if (isNaN(numberValue)) return ""; // ← Fixed: Only return empty string if NaN
+
+  return new Intl.NumberFormat("en-IN", {
+    style: "currency",
+    currency: "INR",
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 2,
+  }).format(numberValue);
+};
+
+export const PriceFilter = ({
+  minPrice,
+  maxPrice,
+  onMinPriceChange,
+  onMaxPriceChange,
+}: Props) => {
+  const handleMinPriceChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const numericalValue = e.target.value.replace(/[^0-9]/g, "");
+    onMinPriceChange(numericalValue);
+  };
+
+  const handleMaxPriceChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const numericalValue = e.target.value.replace(/[^0-9]/g, "");
+    onMaxPriceChange(numericalValue);
+  };
+
+  return (
+    <div className="flex flex-col gap-2">
+      <div className="flex flex-col gap-2">
+        <Label className="font-medium text-base">Minimum Price</Label>
+        <Input
+          type="text"
+          placeholder="₹0"
+          value={minPrice ? formatAsCurrency(minPrice) : ""}
+          onChange={handleMinPriceChange}
+        />
+      </div>
+      <div className="flex flex-col gap-2">
+        <Label className="font-medium text-base">Maximum Price</Label>
+        <Input
+          type="text"
+          placeholder="∞"
+          value={maxPrice ? formatAsCurrency(maxPrice) : ""}
+          onChange={handleMaxPriceChange}
+        />
+      </div>
+    </div>
+  );
+};
